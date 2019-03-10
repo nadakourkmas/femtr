38a5e5f45d97a995e3186a5bd5cdbfc16e70ce68
Nada Kourkmas <nadak8@hotmail.com>
2019-03-10 12:41:33 -0700

    changed db name

--- src/connect.php
+++ src/connect.php
@@ -1,4 +1,4 @@
 <?php
-	$dbhost = "localhost";
-	$dbuser = "root"; $dbpass = "travel"; $db = "femtr";
+	$dbhost = "d13xat1hwxt21t45.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+	$dbuser = "y9yu0d233xbo18n5	"; $dbpass = "hp4r8wjwzitd62lx"; $db = "bw38cd5djbeojtau";
 	$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
